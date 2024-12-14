bc48fe7e008d548294f316734993decdc04dd1c5
Ashwin Chakravartula <f20190650@goa.bits-pilani.ac.in>
2024-12-14 19:52:56 +0100

    Add files via upload

---
+++ turtlebot_nav/launch/launchfile1.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Launch Node 1 : Costmap publisher
+       # Node(
+       #     package='turtlebot_nav',
+       #     executable='costmap_publisher',
+       #     name='costmap_publisher_node',
+       #     output='screen',
+       # ),
+        
+        # Launch Node 2 : Robot Position Subscriber
+        Node(
+            package='turtlebot_nav',
+            executable='sub_robot_position',
+            name='robot_position_node',
+            output='screen'
+        )
+    ])
