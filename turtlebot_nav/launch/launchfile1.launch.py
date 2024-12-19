560f9a86bf987f10698db2002444dca83503fede
Ashwin Chakravartula <f20190650@goa.bits-pilani.ac.in>
2024-12-19 12:55:36 +0100

    Add files via upload

--- turtlebot_nav/launch/launchfile1.launch.py
+++ turtlebot_nav/launch/launchfile1.launch.py
@@ -4,18 +4,34 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         # Launch Node 1 : Costmap publisher
-       # Node(
-       #     package='turtlebot_nav',
-       #     executable='costmap_publisher',
-       #     name='costmap_publisher_node',
-       #     output='screen',
-       # ),
+        Node(
+            package='turtlebot_nav',
+            executable='costmap_publisher',
+            name='costmap_publisher_node',
+            output='screen',
+        ),
         
         # Launch Node 2 : Robot Position Subscriber
         Node(
             package='turtlebot_nav',
             executable='sub_robot_position',
             name='robot_position_node',
-            output='screen'
-        )
+            output='screen',
+        ),
+
+        # Launch Node 3 : A Star Planner
+        Node(
+            package='turtlebot_nav',
+            executable='a_star_planner',
+            name='a_star_planner_node',
+            output='screen',
+        ),
+
+        # Launch Node 4 : Costmap visualizer
+        #Node(
+        #    package='turtlebot_nav',
+        #    executable='costmap_viewer',
+        #    name='costmap_viewer_node',
+        #    output='screen',
+        #),
     ])
